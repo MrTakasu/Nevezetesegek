cdd73acef7da552a0e42785507c296563389292d
MrTakasu <74562169+MrTakasu@users.noreply.github.com>
2022-03-23 14:00:03 +0100

    Add files via upload

---
+++ Európa.py
@@ -0,0 +1,25 @@
+def Europa():
+    print("Párizs 1/ London 2/ Barcelona 3/ Róma 4/ Velence 5")
+    
+    def Párizs():
+        print("Eiffel-torony 1")
+        def Eiffeltorony():
+            print("Az Eiffel-torony (kiejtése: ejfel-torony; franciául: La Tour Eiffel, Loudspeaker.svg kiejtése✩ [la tuʁ‿ɛˈfɛl]) Párizs egyik jelképe. Nevét a tervezőcég tulajdonosáról, Gustave Eiffel mérnökről kapta. Az 1889. évi világkiállításra készült, az eredeti tervek szerint a kiállítás után lebontották volna. A torony összmagassága 324 m, ebből maga a torony 300 m, rászerelve 24 m-re magasodik egy tévé-adóantenna.[1] Első emelete 58, második emelete 116, a harmadik 276 méter magasan van.[2]")
+            
+    def London():
+        print("Big Ben 1")
+        def Bigben():
+            print("A Big Ben a londoni Westminster-palota toronyórájának harangja, illetve az óraszerkezet neve. Az angol parlament keleti szárnyának végén, egy óratoronyban található. Ismert az óra pontosságáról és a 13,5 tonnás harangról.[1] Az óratorony világszerte London, sőt egész Anglia közismert, általános jelképe.")
+            
+    def Barcelona():
+        print("A Szent Család Temploma 1")
+        def Aszentcsaladtemplom():
+            print("A Szent Család-templom (katalánul Sagrada Família), teljes nevén A Szent Család engesztelő temploma (Temple Expiatori de la Sagrada Família) vagy A Jövő Temploma római katolikus templom Spanyolországban, Katalónia fővárosában, Barcelonában, Antoni Gaudí mesterműve.")
+        
+    def Budapest():
+        print("Szent István-bazilika 1")
+        def Szentistvánbazilika():
+            print("A Szent István király bazilika társszékesegyház  egy neoreneszánsz stílusú basilica minor rangú székesegyház Budapest V. kerületében, Lipótvárosban. Ez Magyarország egyik legjelentősebb egyházi épülete, az Esztergom-Budapesti főegyházmegye társszékesegyháza az Esztergomi bazilika mellett. Az épület névadója a magyar államalapító király, Szent István, akinek épségben maradt jobb kezét, a Szent Jobbot ereklyeként itt őrzik. 96 méteres magasságával az ország egyik legmagasabb, az Országház után a főváros második legmagasabb épülete.")
+
+    def Velence():
+        print()
