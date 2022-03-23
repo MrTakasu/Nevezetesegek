ebd18cfb78d974d18312045a841b71647979dcb6
MrTakasu <74562169+MrTakasu@users.noreply.github.com>
2022-03-23 14:32:22 +0100

    Add files via upload

--- Európa.py
+++ Európa.py
@@ -2,24 +2,46 @@ def Europa():
     print("Párizs 1/ London 2/ Barcelona 3/ Róma 4/ Velence 5")
     
     def Párizs():
-        print("Eiffel-torony 1")
+        print("Eiffel-torony 1/ Louvre 2/ Arc de Triomphe(Diadalív) 3")
         def Eiffeltorony():
             print("Az Eiffel-torony (kiejtése: ejfel-torony; franciául: La Tour Eiffel, Loudspeaker.svg kiejtése✩ [la tuʁ‿ɛˈfɛl]) Párizs egyik jelképe. Nevét a tervezőcég tulajdonosáról, Gustave Eiffel mérnökről kapta. Az 1889. évi világkiállításra készült, az eredeti tervek szerint a kiállítás után lebontották volna. A torony összmagassága 324 m, ebből maga a torony 300 m, rászerelve 24 m-re magasodik egy tévé-adóantenna.[1] Első emelete 58, második emelete 116, a harmadik 276 méter magasan van.[2]")
-            
+        def Louvre():
+            print("A Louvre Párizs központi részén, a Szajna jobb partján található nemzeti múzeum, a világ egyik legismertebb és 1793 óta a francia főváros legnagyobb kiállítóhelye. Az európai képzőművészetek gyűjteménye mellett egyiptomi, iszlám, keleti, görög, római és etruszk emlékek is megtekinthetők.")
+        def Diadalív():
+            print("Az Arc de Triomphe  híres párizsi emlékmű, Franciaország fővárosának egyik legismertebb látnivalója, amely I. Napóleon császár és a mindenkori francia hadsereg dicsőségét hirdeti a Champs-Élysées végpontján, a Charles de Gaulle téren.")  
+
     def London():
-        print("Big Ben 1")
+        print("Big Ben 1/ British Museum 2/ Londoni Tower 3")
         def Bigben():
             print("A Big Ben a londoni Westminster-palota toronyórájának harangja, illetve az óraszerkezet neve. Az angol parlament keleti szárnyának végén, egy óratoronyban található. Ismert az óra pontosságáról és a 13,5 tonnás harangról.[1] Az óratorony világszerte London, sőt egész Anglia közismert, általános jelképe.")
-            
+        def Britishmuseum():
+            print("A londoni British Museum egyike a világ legnagyobb, az emberi történelemmel és kultúrával foglalkozó múzeumainak. Gyűjteményei – melyek együttesen több mint 13 millió tárgyból állnak – a kezdetektől a jelenkorig mutatják be az emberi kultúra történetét")    
+        def Londonitower():
+            print("A Tower öbb erődített részből álló középkori épületegyüttes London belvárosában a Temze partján, amely az idők során erődítmény, fegyverraktár, királyi palota és főrendű foglyoknak szánt börtön, valamint levéltár és csillagvizsgáló is volt. I. Jakabig minden király és királynő itt lakott. Még utána is az volt a szokás, hogy az uralkodó a Towerben töltötte a koronázása előtti éjszakát és innen vitték díszkísérettel a Westminsterbe. Jelenleg a Towerben őrzik a brit koronaékszereket, valamint egy gazdag fegyvergyűjteményt.")
+  
     def Barcelona():
-        print("A Szent Család Temploma 1")
+        print("A Szent Család Temploma 1/ Güell park 2/ La Larambla 3")
         def Aszentcsaladtemplom():
             print("A Szent Család-templom (katalánul Sagrada Família), teljes nevén A Szent Család engesztelő temploma (Temple Expiatori de la Sagrada Família) vagy A Jövő Temploma római katolikus templom Spanyolországban, Katalónia fővárosában, Barcelonában, Antoni Gaudí mesterműve.")
-        
+        def Güellpark():
+            print("A Güell park 17 hektáros nyilvános városi park kertekkel és építészeti különlegességekkel. Közigazgatásilag Barcelona Gràcia kerületének La Salud negyedéhez tartozik. Ma Antoni Gaudí épületeinek keretében az UNESCO világörökség részét képezi.")
+        def Larambla():
+            print("A La Rambla gy utca Barcelona központjában. A fákkal szegélyezett sétálóutca 1,2 km-en át húzódik, és a központjában lévő Plaça de Catalunyát köti össze a Port Vellben található Kolumbusz Kristóf emlékművel. A La Rambla képezi a határt a keleti Barri Gòtic és a nyugati El Raval negyedek között.")
+  
     def Budapest():
-        print("Szent István-bazilika 1")
+        print("Szent István-bazilika 1/ Országház 2/ Széchenyi gyógyfürdő 3")
         def Szentistvánbazilika():
             print("A Szent István király bazilika társszékesegyház  egy neoreneszánsz stílusú basilica minor rangú székesegyház Budapest V. kerületében, Lipótvárosban. Ez Magyarország egyik legjelentősebb egyházi épülete, az Esztergom-Budapesti főegyházmegye társszékesegyháza az Esztergomi bazilika mellett. Az épület névadója a magyar államalapító király, Szent István, akinek épségben maradt jobb kezét, a Szent Jobbot ereklyeként itt őrzik. 96 méteres magasságával az ország egyik legmagasabb, az Országház után a főváros második legmagasabb épülete.")
+        def Országház():
+            print("Az Országház Budapest egyik legismertebb középülete, a Magyar Országgyűlés és egyes intézményeinek székhelye. Budapest V. kerületében, a Duna bal partján, a Kossuth Lajos téren található. Bár az épületre vonatkozóan a köznyelvben a Parlament elnevezés is elterjedt, ez valójában az ott ülésező testület, az Országgyűlés idegen eredetű megnevezése.")
+        def Széchenyigyógyfürdő():
+            print("A Széchenyi gyógyfürdő, a pesti szleng becenevén a „Szecska”, Budapest legnagyobb fürdőkomplexuma. Építését 1909-ben kezdték meg Czigler Győző tervei alapján. A Városligetben található fürdő termálvízellátását jelenleg az 1938-ban átadott II. számú, „Szent István” kút, hidegvíz-ellátását 6 darab kisebb kút biztosítja. A komplexum 3 kültéri és 15 beltéri medencével rendelkezik. A fürdőben különböző gyógykezelések vehetők igénybe, és nappali kórházak is működnek.")
 
     def Velence():
-        print()
+        print("Dózsepalota 1/ Szent Márk-székesegyház 2/ Rialto híd 3")
+        def Dozsepalota():
+            print("A dózsepalota az egykori Velencei Köztársaság fejének, a dózsének palotája, Velence egyik legismertebb műemléke, a gótikus világi építészet egyik velencei remeke. A Szent Márk-székesegyház mellett, a Libreriával szemben fekszik a Piazzettán.")
+        def Szentmárkszékesegyház():
+            print("A Szent Márk-székesegyház vagy Szent Márk-bazilika  az olaszországi Velencében, a város központjában, a Szent Márk téren áll. A Velencei patriarkátus főszékesegyháza.")
+        def Rialtóhíd():
+            print("A Rialto híd egyike a négy hídnak amelyek a Canal Grande felett átívelnek, Velence városában. A négy híd közül ez a legidősebb és minden bizonnyal a leghíresebb.")
