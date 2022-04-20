8fdbdbfa8d15f44f3a77f1639dc7e9032188b979
MrTakasu <74562169+MrTakasu@users.noreply.github.com>
2022-04-20 14:57:53 +0200

    Add files via upload
    EZ game

--- Menü.py
+++ Menü.py
@@ -1,16 +1,13 @@
-
-from Amerika import Amerika
-from Európa import Europa
-#from Ázsia import Ázsia
-
 print(" Ha Amerika nevezetességeit akarja látni nyomja meg az egyes gombot. \n Ha Európa nevezetességeit akarja látni nyomja meg a kettes gombot. \n Ha Ázsia nevezetességeit akarja látni nyomja meg a hármas gombot. \n Ha véleményt szeretne írni nyomja meg a négyes gombot ")
 Valasztas = int(input("Ide Írd az értékeket: "))
 if Valasztas == 1:
+    from Amerika import Amerika
     Amerika()
 elif Valasztas == 2:
-    Europa()
+    from Európa import Europa
 elif Valasztas == 3:
-    print(" ")
+    from Asia import Asia
+    Asia()
 elif Valasztas > 4 or Valasztas < 1:
     print("Ez a funkció nem létezik")
 else:
